6e299fdc72cf55493fc4353f6b68c03884a7c041
Zapatannicolas <zapatannicolas@gmail.com>
2026-08-22 17:43:22 -0300

    Agregar clase Festival

--- src/datos/Festival.java
+++ src/datos/Festival.java
@@ -1,5 +1,81 @@
 package datos;
 
+import java.util.Date;
+
 public class Festival {
+	
+	private Long id;
+    private String nombre;
+    private String temporada;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private Set<UnidadVenta> unidadesVenta;
+    private Set<Pedido> pedidos;
+    
+    public Festival() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTemporada() {
+		return temporada;
+	}
+
+	public void setTemporada(String temporada) {
+		this.temporada = temporada;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+    
+	
+	public Set<UnidadVenta> getUnidadesVenta() {
+		return unidadesVenta;
+	}
+
+	public void setUnidadesVenta(Set<UnidadVenta> unidadesVenta) {
+		this.unidadesVenta = unidadesVenta;
+	}
+
+	public Set<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(Set<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	@Override
+	public String toString() {
+		return "Festival [id=" + id + ", nombre=" + nombre
+				+ ", temporada=" + temporada + ", fechaInicio=" + fechaInicio
+				+ ", fechaFin=" + fechaFin + "]";
+	}
 
 }
