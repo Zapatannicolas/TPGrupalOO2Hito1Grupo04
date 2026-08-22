3e6e2d3927dd982cd9fc45a8cf2baf968693b795
Zapatannicolas <zapatannicolas@gmail.com>
2026-08-22 17:49:28 -0300

    Agregar clase Festival

--- src/dao/FestivalDao.java
+++ src/dao/FestivalDao.java
@@ -1,5 +1,143 @@
 package dao;
 
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import datos.Festival;
+
 public class FestivalDao {
 
+	private static Session session;
+	private Transaction tx;
+
+	private void iniciaOperacion() throws HibernateException {
+		session = HibernateUtil.getSessionFactory().openSession();
+		tx = session.beginTransaction();
+	}
+
+	private void manejaExcepcion(HibernateException he) throws HibernateException {
+		tx.rollback();
+		throw new HibernateException("ERROR en la capa de acceso a datos", he);
+	}
+
+	public int agregar(Festival objeto) {
+		int id = 0;
+		try {
+			iniciaOperacion();
+			id = Integer.parseInt(session.save(objeto).toString());
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+		return id;
+	}
+
+	public void actualizar(Festival objeto) {
+		try {
+			iniciaOperacion();
+			session.update(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+	}
+
+	public void eliminar(Festival objeto) {
+		try {
+			iniciaOperacion();
+			session.delete(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+	}
+
+	public Festival traer(long idFestival) {
+		Festival objeto = null;
+		try {
+			iniciaOperacion();
+			objeto = (Festival) session.get(Festival.class, idFestival);
+		} finally {
+			session.close();
+		}
+		return objeto;
+	}
+
+	public Festival traer(String nombre) {
+		Festival objeto = null;
+		try {
+			iniciaOperacion();
+			objeto = (Festival) session.createQuery(
+					"from Festival f where f.nombre=:nombre")
+					.setParameter("nombre", nombre)
+					.uniqueResult();
+		} finally {
+			session.close();
+		}
+		return objeto;
+	}
+
+	public List<Festival> traer() throws HibernateException {
+		List<Festival> lista = null;
+		try {
+			iniciaOperacion();
+			lista = session.createQuery(
+					"from Festival f order by f.nombre asc",
+					Festival.class).getResultList();
+		} finally {
+			session.close();
+		}
+		return lista;
+	}
+
+	public Festival traerFestivalYUnidades(long idFestival) throws HibernateException {
+		Festival objeto = null;
+		try {
+			iniciaOperacion();
+
+			String hql = "from Festival f where f.id=:idFestival";
+
+			objeto = (Festival) session.createQuery(hql)
+					.setParameter("idFestival", idFestival)
+					.uniqueResult();
+
+			Hibernate.initialize(objeto.getUnidadesVenta());
+
+		} finally {
+			session.close();
+		}
+		return objeto;
+	}
+
+	public Festival traerFestivalYPedidos(long idFestival) throws HibernateException {
+		Festival objeto = null;
+		try {
+			iniciaOperacion();
+
+			String hql = "from Festival f where f.id=:idFestival";
+
+			objeto = (Festival) session.createQuery(hql)
+					.setParameter("idFestival", idFestival)
+					.uniqueResult();
+
+			Hibernate.initialize(objeto.getPedidos());
+
+		} finally {
+			session.close();
+		}
+		return objeto;
+	}
 }
--- src/negocio/FestivalABM.java
+++ src/negocio/FestivalABM.java
@@ -1,5 +1,52 @@
 package negocio;
 
+import java.util.Date;
+import java.util.List;
+
+import dao.FestivalDao;
+import datos.Festival;
+
 public class FestivalABM {
 
+	FestivalDao dao = new FestivalDao();
+
+	public Festival traer(long id) {
+		return dao.traer(id);
+	}
+
+	public Festival traer(String nombre) {
+		return dao.traer(nombre);
+	}
+
+	public int agregar(String nombre, String temporada, Date fechaInicio, Date fechaFin) {
+		Festival f = new Festival();
+
+		f.setNombre(nombre);
+		f.setTemporada(temporada);
+		f.setFechaInicio(fechaInicio);
+		f.setFechaFin(fechaFin);
+
+		return dao.agregar(f);
+	}
+
+	public void modificar(Festival f) {
+		dao.actualizar(f);
+	}
+
+	public void eliminar(long id) {
+		Festival f = dao.traer(id);
+		dao.eliminar(f);
+	}
+
+	public List<Festival> traer() {
+		return dao.traer();
+	}
+
+	public Festival traerFestivalYUnidades(long id) {
+		return dao.traerFestivalYUnidades(id);
+	}
+
+	public Festival traerFestivalYPedidos(long id) {
+		return dao.traerFestivalYPedidos(id);
+	}
 }
