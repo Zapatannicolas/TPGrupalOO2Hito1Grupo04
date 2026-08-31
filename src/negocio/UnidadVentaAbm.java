5f8b0e2663be5d051b38af0b96366f396d3c8acf
Ignacio Aldonate <110785590+ignacioAldonate1@users.noreply.github.com>
2026-08-31 20:16:24 -0300

    Update UnidadVentaAbm.java
    - Se crea el agregarFoodTruck y agregarPuestoDesarmable por separado
    - Se agrega el atributo festival en estos constructores  
    - Agregue el método traerUnidadVentaYStaff (set)
    - Modificacion del método traerUnidadYPedidos ya que retornaba el dao del traer standard

--- src/negocio/UnidadVentaAbm.java
+++ src/negocio/UnidadVentaAbm.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import dao.UnidadVentaDao;
 import datos.UnidadVenta;
+import datos.Festival;
 import datos.FoodTruck;
 import datos.Persona;
 import datos.PuestoDesarmable;
@@ -11,24 +12,37 @@ import datos.PuestoDesarmable;
 public class UnidadVentaAbm {
 	
 	UnidadVentaDao dao = new UnidadVentaDao();
-
-	public int agregar(UnidadVenta objeto) throws Exception{
-		if(traer(objeto.getIdUnidadVenta())!= null) throw new Exception("Ya existe una unidad de venta con el mismo codigo");
-	    return dao.agregar(objeto);
-	}
+	
+	public int agregarFoodTruck(String nombreComercial, double superficie, String codigoUnico, Persona responsable, Festival festival, String patente, boolean usaLuz) throws Exception{
+        if(traer(codigoUnico)!=null) throw new Exception("ERROR ya existe un food Truck con el mismo codigo");
+		return dao.agregar(new FoodTruck(nombreComercial, superficie, codigoUnico, responsable, festival, patente, usaLuz));
+	};
+	
+	public int agregarPuestoDesarmable(String nombreComercial, double superficie, String codigoUnico, Persona responsable, Festival festival, int cantidad, int tiempoMontaje) throws Exception {
+        if(traer(codigoUnico)!=null) throw new Exception("ERROR ya existe un puesto desarmable con el mismo codigo");
+		return dao.agregar(new PuestoDesarmable(nombreComercial, superficie, codigoUnico, responsable, festival, cantidad, tiempoMontaje));
+	};
 	
 	public UnidadVenta traer(long idUnidadVenta) {
 		return dao.traer(idUnidadVenta);
 	}
 
+	public UnidadVenta traer(String codigoUnico) {
+		return dao.traer(codigoUnico);
+	}
+	
 	public List<UnidadVenta> traer() {
 		return dao.traer();
 	}
 	
 	public UnidadVenta traerUnidadVentaYPedidos(long idUnidadVenta) {
-		return dao.traer(idUnidadVenta);
+		return dao.traerUnidadVentaYPedidos(idUnidadVenta);
 	}
 
+	public UnidadVenta traerUnidadVentaYStaff(long idUnidadVenta) {
+		return dao.traerUnidadVentaYStaff(idUnidadVenta);
+	}
+	
 	public void modificar(UnidadVenta u) throws Exception{
 		UnidadVenta encontrada = traer(u.getIdUnidadVenta());
 		if(encontrada !=null && encontrada.getIdUnidadVenta() != u.getIdUnidadVenta()) throw new Exception ("ERROR el codigo unico ya esta en uso");
